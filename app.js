4dfe101eba5ebdff0f87f50a4bc610e39c4c8903
erkitammeaid <erkipoiss01@gmail.com>
2021-10-27 14:12:50 +0300

    score table

--- app.js
+++ app.js
@@ -18,7 +18,47 @@ function rng() {
             if (el.dataset.state == 'active'){
                 var x = Math.floor((Math.random() * 6) + 1);
                 el.innerText = x ;
+                diceState[el.dataset.index].value = x
             }
         });
     }
 }
+
+let diceState = [
+    {
+        value: undefined,
+        status: 'active'
+    },
+    {
+        value: undefined,
+        status: 'active'
+    },
+    {
+        value: undefined,
+        status: 'active'
+    },
+    {
+        value: undefined,
+        status: 'active'
+    },
+    {
+        value: undefined,
+        status: 'active'
+    },
+]
+
+function scoreCounter(index){
+    return diceState.reduce((counter, el)=>{
+        el.value == index ? counter++ : counter;
+        return counter;
+    }, 0) * index;
+}
+
+const scoreCells = document.querySelectorAll('.score');
+scoreCells.forEach( el => {
+    el.addEventListener('click', e => {
+        if (el.dataset.scoreType == 'count'){
+            el.innerText = scoreCounter(el.dataset.scoreIndex)
+        }
+    })
+})
